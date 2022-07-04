115d06e80a350796c26dcf081f7887cccf805268
Varun <varun@qubicle.co.in>
2022-07-04 17:35:49 +0530

    env

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect('mongodb://localhost:27017/test?retryWrites=false', {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,			
 		});
